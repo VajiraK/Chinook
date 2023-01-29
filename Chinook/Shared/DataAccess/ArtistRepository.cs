aed16467599ec1a27ed524a439d48b99d669b802
Vajira Kulatunga <vajira.kulatunga@pearson.com>
2023-01-29 18:59:37 +0530

    Move ArtistPage.razor data access to repository

--- Chinook/Shared/DataAccess/ArtistRepository.cs
+++ Chinook/Shared/DataAccess/ArtistRepository.cs
@@ -1,6 +1,22 @@
-﻿namespace Chinook.Shared.DataAccess
+﻿using Chinook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chinook.Shared.DataAccess
 {
     public class ArtistRepository : IArtistRepository
     {
+        readonly IDbContextFactory<ChinookContext> _DbFactory;
+
+        public ArtistRepository(IDbContextFactory<ChinookContext> dbFactory)
+        {
+            _DbFactory = dbFactory;
+        }
+
+        public async Task<Artist> GetArtist(long ArtistId)
+        {
+            var DbContext = await _DbFactory.CreateDbContextAsync();
+            var Artist = DbContext.Artists.SingleOrDefault(a => a.ArtistId == ArtistId);
+            return Artist;
+        }
     }
 }
--- Chinook/Shared/DataAccess/IArtistRepository.cs
+++ Chinook/Shared/DataAccess/IArtistRepository.cs
@@ -1,6 +1,10 @@
-﻿namespace Chinook.Shared.DataAccess
+﻿using Chinook.Models;
+
+namespace Chinook.Shared.DataAccess
 {
     public interface IArtistRepository
     {
+        Task<Artist> GetArtist(long ArtistId);
+
     }
 }
